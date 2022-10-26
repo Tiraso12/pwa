403f621c7e33da9a80c514a5783da66fd13876fe
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-25 22:19:38 -0400

    added the right files from the challenge repo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,35 +18,12 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin'
-      })
       
     ],
 
     module: {
       rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-      },
-      {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-      },
-      {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-              loader: 'babel-loader',
-              options: {
-                  presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                  ]
-              }
-          }
-      }
+        
       ],
     },
   };
