ecd703d9aa7b7860f1ae659f1104f4b43814b278
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-31 21:32:34 -0400

    put db implemente not showing error on console anymore

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('POST to database');
+  //connect to database
+  const appDb = await openDB('jate', 1);
+  //creates new transaction and specify the store data privileges
+  const tx = appDb.transaction('jate', 'readwrite');
+  //opens the object
+  const store = tx.objectStore('jate');
+  //use the .add() method on the store and pass in the content
+  const request = store.add({ id: 1 , value: content})
+  //gets confirmation of the request
+  const result = await request;
+  console.log('data saved', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+};
 
 initdb();
