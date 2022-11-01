922d06430eff515197e90d2fdfaac78a1d64fa0a
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-31 21:42:05 -0400

    added the get method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -30,6 +30,24 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
+  console.log('GET from the database');
+
+  // create a connection to the indexDB database & the version we want to use
+  const appDb = await openDB('jate', 1);
+
+  // create a new transaction and specify the store and data privileges
+  const tx = appDb.transaction('jate', 'readonly');
+
+  // open up the desired object store
+  const store = tx.objectStore('jate');
+
+  // use the .getAll() method to get all data in the database
+  const request = store.get(1);
+
+  // get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
